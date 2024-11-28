93caad27b2686dc57870fc3d2db1e62104310428
Bhosale Prathmesh Shivaji <131182243+BhosalePrathmesh@users.noreply.github.com>
2024-11-28 15:33:17 +0530

    urls.py
    URL configuration

--- service_requests/urls.py
+++ service_requests/urls.py
@@ -1,1 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('submit/', views.submit_request, name='submit_request'),
+    path('track/', views.track_requests, name='track_requests'),
+]
 
